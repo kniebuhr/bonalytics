171a887662022f3c9b173edde3f4e655d65c2353
kniebuhr <gk.niebuhr@gmail.com>
2017-06-13 16:44:53 -0300

    new stuff

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-const pg = require('pq');
 var express = require('express');
 var app = express();
-var connectionString = "postgres://bonamango:teste1234@bonamango.postgresql.dbaas.com.br:5432/bonamango?ssl=true"
+app.listen(2222);
 
-app.get('/db', function (request, response) {
-  pg.connect(connectionString, function(err, client, done) {
-    client.query('select CAST(teste AS character varying(10)) AS id, teste3 as name FROM test_table', function(err, result) {
-      done();
-      if (err)
-        { console.error(err); response.send("Error " + err); }
-      else
-        { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
+var db = require('./database.js');
+
+app.post('consumer/signup', db.postLogin);
+app.get('/login', db.getLogin);
+app.get('/search', db.getSearch);
+app.get('/random', db.getRandom);
+app.post('/trade', db.postTrade);
