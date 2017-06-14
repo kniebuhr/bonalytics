80fa8d08a361038bb0bac6b2e30440be95221bca
kniebuhr <gk.niebuhr@gmail.com>
2017-06-13 22:52:20 -0300

    procedures

--- database.js
+++ database.js
@@ -18,12 +18,19 @@ module.exports = {
 };
 
 function postLogin(req, res, next) {
-  db.any('insert into the_user(user_id, name_first_name, name_last_name, email, password, user_status_id) values((select max(user_id) from the_user)+1, $1, $2, $3, $4, 1)', [req.query.f, req.query.l, req.query.e, req.query.p])
+  db.func('consumerSignUp', [req.query.f, req.query.l, req.query.e, req.query.p])
     .then(function (data) {
-      res.status(201).json({
-        status: "success",
-        message: "created user"
-      })
+      if(data == -1){
+        res.status(400).json({
+          status: "fail",
+          message: "email already in use"
+        });
+      } else {
+        res.status(201).json({
+          status: "success",
+          message: "created user"
+        });
+      }
     })
     .catch(function (err){
       return next(err);
@@ -31,23 +38,12 @@ function postLogin(req, res, next) {
 }
 
 function getRandom(req, res, next) {
-  db.any('select picture, name, price, symbol as unit, rate, min_fraction, description, vendor_id, username as vendor_username, (rate_sum/rate_quantity) as vendor_rate from item join (select username, vendor_id, rate_sum, rate_quantity from vendor join the_user on vendor_id) on vendor_id join unit on unit_id order by random() limit 5')
+  db.func('randomItens', req.params.n)
     .then(function (data) {
-      var json = data;
-      for(let item of json){
-        db.any('select name from tag_cult join item_tag on tag_id where culture_id = 1 and item_id = $1#', item.item_id)
-          .then(function (data2){
-            item.tag = data2;
-        })
-        .catch(function (err) {
-          return next(err);
-        });
-      }
-      res.status(200)
-        .json({
+      res.status(200).json({
           status: 'success',
           data: json,
-          message: 'Retrieved itens that have parameter in its name'
+          message: 'Retrieved random itens'
       });
     })
     .catch(function (err) {
@@ -56,24 +52,20 @@ function getRandom(req, res, next) {
 };
 
 function getSearch(req, res, next) {
-  db.any('select picture, name, price, symbol as unit, rate, min_fraction, description, vendor_id, name_first_name as vendor_username, (rate_sum/rate_quantity) as vendor_rate from product join (select name_first_name, vendor_id, rate_sum, rate_quantity from vendor join the_user on vendor_id) on vendor_id join unit on unit_id where name like \'%$1#%\'', req.query.s)
-    .then(function (data) {
-      var json = data;
-      for(let item of json){
-        db.any('select name from tag_cult join item_tag on tag_id where culture_id = 1 and item_id = $1#', item.item_id)
-          .then(function (data2){
-            item.tag = data2;
+  db.func('itemSearch', req.query.s)
+    .then(function (data){
+      if(isEmpty(data)){
+        res.status(404).json({
+          status: 'fail',
+          message: 'no products with string in name, tags or category'
         })
-        .catch(function (err) {
-          return next(err);
-        });
-      }
-      res.status(200)
-        .json({
+      } else {
+        res.status(200).json({
           status: 'success',
-          data: json,
-          message: 'Retrieved itens that have parameter in its name'
-      });
+          data: data,
+          message: 'returned products with string in name, tags or category'
+        })
+      }
     })
     .catch(function (err) {
       return next(err);
@@ -81,51 +73,26 @@ function getSearch(req, res, next) {
 };
 
 function getLogin(req, res, next){
-  var login = req.query.e, at = "@";
-
-  if(login.indexOf(at) == -1){
-    db.one('select user_id, name_first_name, name_last_name from the_user where user_id = $1# and password = $2', [login, req.query.p])
-      .then(function (data) {
-        if(isEmpty(data)){
-          res.status(404)
-            .json({
-              status: 'fail',
-              message: '404 USER NOT FOUND'
+  db.func('login', [login, req.query.p])
+    .then(function (data) {
+      if (isEmpty(data)){
+        res.status(404)
+          .json({
+            status: 'fail',
+            message: '404 USER NOT FOUND'
           });
-        } else {
-          res.status(200)
-            .json({
-              status: 'success',
-              data: data,
-              message: 'user found and id returned'
-            });
-        }
-      })
-      .catch(function (err) {
-        return next(err);
-      });
-  } else {
-    db.proc('login', [login, req.query.p])
-      .then(function (data) {
-        if (isEmpty(data)){
-          res.status(404)
-            .json({
-              status: 'fail',
-              message: '404 USER NOT FOUND'
-            });
-        } else {
-          res.status(200)
-            .json({
-              status: 'success',
-              data: data,
-              message: 'user found and id returned'
-            });
-        }
-      })
-      .catch(function (err) {
-        return next(err);
-      });
-  }
+      } else {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: data,
+            message: 'user found and id returned'
+          });
+      }
+    })
+    .catch(function (err) {
+      return next(err);
+    });
 };
 
 function postTrade(req, res, next) {
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-app.listen(2222);
+app.listen(8122);
 
 var db = require('./database.js');
 
-app.post('consumer/signup', db.postLogin);
+app.post('/consumer/signup', db.postLogin);
 app.get('/login', db.getLogin);
 app.get('/search', db.getSearch);
-app.get('/random', db.getRandom);
+app.get('/random/:n', db.getRandom);
 app.post('/trade', db.postTrade);
